22e89fddd1bace38ddd118addced2fc42591a65a
MrMatch246 <s8daerce@stud.uni-saarland.de>
2022-08-16 18:58:12 +0200

    fixed job failed error message to not give a list with a single entry

--- fuzzware_pipeline/workers/pool.py
+++ fuzzware_pipeline/workers/pool.py
@@ -284,7 +284,7 @@ class WorkerPool:
 
                 if job_status == 'failed':
                     job_args=[str(arg) for arg in job.args]
-                    self.parent.add_warning_line("job failed: {}, started at: {}, args: {}, exc_info: {}".format(job.func_name, job.started_at, job_args[0], job.exc_info))
+                    self.parent.add_warning_line("job failed: {}, started at: {}, args: {}, exc_info: {}".format(job.func_name, job.started_at, job_args[0][0], job.exc_info))
                 self.jobs.remove(job)
                 new_job_timing_entries.append((job.func_name, job_status, job.enqueued_at, job.started_at, job.ended_at))
 
